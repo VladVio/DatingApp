5f88283940746a81a53eb6b08f23f2f0eeb40dba
BuildTools <unconfigured@null.spigotmc.org>
2024-01-04 21:41:59 +0200

    End of section 5

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  model:any={}
+  @Output() cancelRegister=new EventEmitter();
+  constructor(private accountService:AccountService){
+  }
+  ngOnInit(){
+
+  }
+  register(){
+    this.accountService.register(this.model).subscribe({
+      next:()=>{
+        this.cancel();
+      },
+      error:error=>console.log(error)
+    })
+  }
+  cancel(){
+    this.cancelRegister.emit(false)
+  }
+}
