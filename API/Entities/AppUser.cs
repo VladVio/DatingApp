8ab30ff2298b9fe165075c09699f804d8489fe18
BuildTools <unconfigured@null.spigotmc.org>
2024-02-20 21:27:00 +0200

    End of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -20,7 +20,8 @@ public class AppUser
     public string City {get;set;}
     public string County {get;set;}
     public List<Photo>Photos {get;set;} =new();
-
+     public List<UserLike> LikedByUsers {get;set;}
+      public List<UserLike> LikedUsers {get;set;}
     // public int GetAge(){
     //     return DateOfBirth.CalculateAge();
     // }
